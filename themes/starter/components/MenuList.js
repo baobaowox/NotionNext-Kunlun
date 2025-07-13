b4a14cb46867703209c6be08042cd170b0090404
baobaowox <140580246+baobaowox@users.noreply.github.com>
2025-07-14 00:20:31 +0800

    Update MenuList.js

--- themes/starter/components/MenuList.js
+++ themes/starter/components/MenuList.js
@@ -11,10 +11,8 @@ import { MenuItem } from './MenuItem'
 export const MenuList = props => {
   const { customNav, customMenu } = props
   const { locale } = useGlobal()
-
   const [showMenu, setShowMenu] = useState(false) // 控制菜单展开/收起状态
   const router = useRouter()
-
   let links = [
     {
       icon: 'fas fa-archive',
@@ -41,30 +39,24 @@ export const MenuList = props => {
       show: siteConfig('HEO_MENU_TAG')
     }
   ]
-
   if (customNav) {
     links = customNav.concat(links)
   }
-
   // 如果 开启自定义菜单，则覆盖Page生成的菜单
   if (siteConfig('CUSTOM_MENU', BLOG.CUSTOM_MENU)) {
     links = customMenu
   }
-
   const toggleMenu = () => {
     setShowMenu(!showMenu) // 切换菜单状态
   }
-
   useEffect(() => {
     setShowMenu(false)
   }, [router])
-
   if (!links || links.length === 0) {
     return null
   }
-
   return (
-    <div>
+    <div className="ml-auto">
       {/* 移动端菜单切换按钮 */}
       <button
         id='navbarToggler'
@@ -76,13 +68,12 @@ export const MenuList = props => {
         <span className='relative my-[6px] block h-[2px] w-[30px] bg-white duration-200 transition-all'></span>
         <span className='relative my-[6px] block h-[2px] w-[30px] bg-white duration-200 transition-all'></span>
       </button>
-
       <nav
         id='navbarCollapse'
         className={`absolute right-4 top-full w-full max-w-[250px] rounded-lg bg-white py-5 shadow-lg dark:bg-dark-2 lg:static lg:block lg:w-full lg:max-w-full lg:bg-transparent lg:px-4 lg:py-0 lg:shadow-none dark:lg:bg-transparent xl:px-6 ${
           showMenu ? '' : 'hidden'
         }`}>
-        <ul className='blcok lg:flex 2xl:ml-20'>
+        <ul className='block lg:flex lg:justify-end lg:ml-auto 2xl:ml-20'>
           {links?.map((link, index) => (
             <MenuItem key={index} link={link} />
           ))}
